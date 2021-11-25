db99f201384ba16c1657b36a99e2417956c31099
Arsen <arsen.avetisyan.2000@mail.ru>
2021-11-25 09:08:07 +0300

    change components

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,39 @@
 <template>
-  <div>
-    <ProductsList :products="products"/>
-  </div>
+  <section class="catalog">
+    <ProductsList :products="products" />
+    <BasePagination :page="page" :count="countProducts" :per-page="productsPerPage" />
+  </section>
 </template>
 
 <script>
 
 import products from './data/products';
 import ProductsList from './components/ProductsList.vue';
+import BasePagination from './components/BasePagination.vue';
 
 export default {
   name: 'App',
 
   components: {
     ProductsList,
+    BasePagination,
   },
 
   data() {
     return {
-      products,
+      page: 1,
+      productsPerPage: 3,
     };
   },
 
+  computed: {
+    products() {
+      const offset = (this.page - 1) * this.productsPerPage;
+      return products.slice(offset, offset + this.productsPerPage);
+    },
+    countProducts() {
+      return products.length;
+    },
+  },
 };
 </script>
