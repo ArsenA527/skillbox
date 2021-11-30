a9ae95c8e6cb7f1a6887423228aa83df199fe175
Arsen <arsen.avetisyan.2000@mail.ru>
2021-11-30 08:24:00 +0300

    complete lesson 4.5 part1

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,28 @@
+/* eslint-disable max-len */
 <template>
-  <section class="catalog">
-    <ProductsList :products="products" />
-    <BasePagination v-model="page" :count="countProducts" :per-page="productsPerPage" />
-  </section>
+  <main class="content container">
+    <div class="content__top content__top--catalog">
+      <h1 class="content__title">
+        Каталог
+      </h1>
+      <span class="content__info">
+        152 товара
+      </span>
+    </div>
+
+    <div class="content__catalog">
+      <ProductsFilter
+        :price-from.sync="filterPriceFrom"
+        :price-to.sync="filterPriceTo"
+        :category-id.sync="filterCategoryId"
+      />
+      <section class="catalog">
+        <ProductsList :products="products" />
+        <BasePagination v-model="page" :count="countProducts" :per-page="productsPerPage" />
+      </section>
+    </div>
+  </main>
+
 </template>
 
 <script>
@@ -10,6 +30,7 @@
 import products from './data/products';
 import ProductsList from './components/ProductsList.vue';
 import BasePagination from './components/BasePagination.vue';
+import ProductsFilter from './components/ProductsFilter.vue';
 
 export default {
   name: 'App',
@@ -17,22 +38,41 @@ export default {
   components: {
     ProductsList,
     BasePagination,
+    ProductsFilter,
   },
 
   data() {
     return {
+      filterPriceFrom: 0,
+      filterPriceTo: 0,
+      filterCategoryId: 0,
       page: 1,
       productsPerPage: 3,
     };
   },
 
   computed: {
+    filteredProducts() {
+      let filterProducts = products;
+      if (this.filterPriceFrom > 0) {
+        filterProducts = filterProducts.filter((product) => product.price > this.filterPriceFrom);
+      }
+      if (this.filterPriceTo > 0) {
+        filterProducts = filterProducts.filter((product) => product.price < this.filterPriceTo);
+      }
+      if (this.filterCategoryId) {
+        // eslint-disable-next-line max-len
+        filterProducts = filterProducts.filter((product) => product.categoryId === this.filterCategoryId);
+      }
+      return filterProducts;
+    },
+
     products() {
       const offset = (this.page - 1) * this.productsPerPage;
-      return products.slice(offset, offset + this.productsPerPage);
+      return this.filteredProducts.slice(offset, offset + this.productsPerPage);
     },
     countProducts() {
-      return products.length;
+      return this.filteredProducts.length;
     },
   },
 };
--- src/data/products.js
+++ src/data/products.js
@@ -1,90 +1,120 @@
 export default [
 
   {
+    id: 1,
+    categoryId: 4,
     title: 'Радионяня Motorola MBP16',
     price: 3690,
     imgsrc: '/img/radio.jpg',
   },
 
   {
+    id: 2,
+    categoryId: 1,
     title: 'Ультразвуковая зубная щётка Playbrush Smart Sonic',
     price: 5660,
     imgsrc: '/img/toothbrush.jpg',
   },
 
   {
+    id: 3,
+    categoryId: 2,
     title: 'Смартфон Xiaomi Mi Mix 3 6/128GB',
     price: 21790,
     imgsrc: '/img/phone.jpg',
   },
 
   {
+    id: 4,
+    categoryId: 4,
     title: 'Электроскейт Razor Cruiser',
     price: 24690,
     imgsrc: '/img/board.jpg',
   },
 
   {
+    id: 5,
+    categoryId: 2,
     title: 'Смартфон Xiaomi Mi A3 4/64GB Android One',
     price: 14960,
     imgsrc: '/img/phone-2.jpg',
   },
 
   {
+    id: 6,
+    categoryId: 2,
     title: 'Смартфон Xiaomi Redmi 6/128GB',
     price: 8960,
     imgsrc: '/img/phone-3.jpg',
   },
 
   {
+    id: 7,
+    categoryId: 3,
     title: 'Электрический дрифт-карт Razor Crazy Cart',
     price: 39900,
     imgsrc: '/img/bicycle.jpg',
   },
 
   {
+    id: 8,
+    categoryId: 3,
     title: 'Гироскутер Razor Hovertrax 2.0',
     price: 34900,
     imgsrc: '/img/wheels.jpg',
   },
 
   {
+    id: 9,
+    categoryId: 3,
     title: 'Детский трюковой самокат Razor Grom',
     price: 4990,
     imgsrc: '/img/scooter.jpg',
   },
 
   {
+    id: 10,
+    categoryId: 3,
     title: 'Роллерсёрф Razor RipStik Air Pro',
     price: 6690,
     imgsrc: '/img/ripstik.jpg',
   },
 
   {
+    id: 11,
+    categoryId: 5,
     title: 'Наушники AirPods с беспроводным зарядным футляром',
     price: 16560,
     imgsrc: '/img/airpods.jpg',
   },
 
   {
+    id: 12,
+    categoryId: 5,
     title: 'Наушники Sony',
     price: 30690,
     imgsrc: '/img/headphones.jpg',
   },
 
   {
+    id: 13,
+    categoryId: 3,
     title: 'Груша пневматическая на растяжках Speed',
     price: 7990,
     imgsrc: '/img/speed-bag.webp',
   },
 
   {
+    id: 14,
+    categoryId: 3,
     title: 'Боксерская груша-шар FightTech Proffi Leather, 45 кг',
     price: 19000,
     imgsrc: '/img/fightech.webp',
   },
 
   {
+    id: 15,
+    categoryId: 3,
     title: 'Крестовая стойка на 2 боксерских мешка Green Hill',
     price: 59990,
     imgsrc: '/img/suspension-boxing.webp',
