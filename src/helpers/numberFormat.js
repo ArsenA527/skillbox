399abcf2414efa7f3d95190b5a79d244ec0b2180
Arsen <arsen.avetisyan.2000@mail.ru>
2022-01-04 06:19:19 +0300

    fix numberFormat

--- src/helpers/numberFormat.js
+++ src/helpers/numberFormat.js
@@ -1,6 +1,7 @@
 export default function numberFormat(value) {
-  if (typeof (+value) !== 'number') {
-    return 0;
+  const number = +value;
+  if (Number.isNaN(number) || typeof number !== 'number') {
+    return 'данные отстутствуеют';
   }
   return new Intl.NumberFormat().format(value);
 }
